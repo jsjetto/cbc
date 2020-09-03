f767b2f9675917b6d9476684c7b05c0db2ebcbd6
jsjetto <jsjetto@gmail.com>
2020-09-03 10:58:46 -0300

    Update get-promo.js

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,11 +1,10 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
-import { processEnv } from 'next/dist/lib/load-env-config'
 import { fromBase64 } from '../../util/base64'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
 export default async (req, res) => {
-  console.log(fromBase64(process.env.SHEET_PRIVATE_KEY))
+
   try {
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
