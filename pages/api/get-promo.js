367c501607d89cea1541461c5a4b3f5982a11fa2
jsjetto <jsjetto@gmail.com>
2020-09-03 10:48:19 -0300

    Add base64 estable

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,12 +1,8 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 import { processEnv } from 'next/dist/lib/load-env-config'
+import { fromBase64 } from '../../util/base64'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
-
-const fromBase64 = value => {
-  const buff = Buffer.from(value, 'base64');
-  return buff.toString('ascii');
-}
 
 export default async (req, res) => {
   console.log(fromBase64(process.env.SHEET_PRIVATE_KEY))
